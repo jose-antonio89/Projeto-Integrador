83ee6dc94f3b0fe7a9e71a5b06c983b3a2b2c154
José Antônio <jose.felix3@fatec.sp.gov.br>
2025-11-19 10:28:32 -0300

    Retirando versões antigas

--- Site Pi - Freelance v0.31/js/scriptindex.js
+++
@@ -1,8 +0,0 @@
-
-
-
-if (localStorage.getItem('darkMode') === 'enabled') {
-    document.body.classList.add('dark-mode');
-    const icon = darkModeToggle.querySelector('i');
-    icon.classList.replace('fa-moon', 'fa-sun');
-}
